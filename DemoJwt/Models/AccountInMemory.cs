95e1be6f64822054891998a1151c02ab7c04d40a
VuNgo <vu.ngo@tekcent.com>
2017-07-02 20:03:37 +0700

    Update Code

---
+++ DemoJwt/Models/AccountInMemory.cs
@@ -0,0 +1,22 @@
+﻿namespace DemoJwt.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class AccountInMemory
+    {
+        public static IList<Account> ArrayAccount = new List<Account>();
+
+        static AccountInMemory()
+        {
+            ArrayAccount.Add(new Account
+            {
+                Id = Guid.NewGuid().ToString("n"),
+                FirstName = "Butter",
+                LastName = "Ngo",
+                UserName ="admin",
+                Password = "123456"
+            });
+        }
+    }
+}
